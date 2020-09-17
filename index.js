e7d29b25aa48d2278d66cb46fd59132d2f22b759
Diman Kostadinov <d78mdd@gmail.com>
2020-09-17 09:29:34 +0000

    use db

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
 const app = express();
+
+const Database = require("@replit/database")
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello Express app!')
@@ -8,4 +11,15 @@ app.get('/', (req, res) => {
 
 app.listen(3000, () => {
   console.log('server started');
+});
+
+
+const db = new Database()
+
+
+db.set("january", "31").then(() => {});
+
+
+db.list().then(keys => {
+  console.log(keys)  
 });
