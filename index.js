33e9ed670a57c72e1ea0f5855770eb9557ea313a
Diman Kostadinov <d78mdd@gmail.com>
2020-09-18 10:29:45 +0000

    versions

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/*
+some info
+https://docs.microsoft.com/en-us/office/troubleshoot/excel/determine-a-leap-year
+
+? possible versions
+1 period is withing twelve consecutive months
+2 period extends to next year
+3 period can span multiple years
+.. ?5 include hours and minutes and seconds
+*/
+
 const express = require('express');
 const app = express();
 
@@ -24,13 +35,27 @@ app.get('/', (req, res) => {
   let october = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
   let november = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
   let december = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
-  /*
-  if (true) {
-    february = february2
+  
+  
+  
+/*
+To determine whether a year is a leap year, follow these steps:
+
+1. If the year is evenly divisible by 4, go to step 2. Otherwise, go to step 5.
+2. If the year is evenly divisible by 100, go to step 3. Otherwise, go to step 4.
+3. If the year is evenly divisible by 400, go to step 4. Otherwise, go to step 5.
+4. The year is a leap year (it has 366 days).
+5. The year is not a leap year (it has 365 days).
+*/
+
+  if () {
+    february = february2  //a leap year
   } else {
-    february = february1
+    february = february1  //not a leap year
   }
-  */
+  
+  
+  
   let year = [...january, ...february, ...march, ...april, ...may, ...june, ...july, ...august, ...september, ...october, ...november, ...december]
 
   console.log(year)
