eec835edc1749f78a63ec6bbbb61260f2731ac4c
Diman Kostadinov <d78mdd@gmail.com>
2020-10-16 13:41:23 +0000

    beta2 / working version

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ possible methods
 
 
 
-
 const express = require('express');
 const app = express();
 
@@ -38,7 +37,6 @@ app.get('/', (req, res) => {
 });
 
 
-  
 
 /*
 pass the dates
@@ -56,53 +54,54 @@ by post req.body using a form
 
 
 
-
 app.post("/date", function(req, res){
   // "method" 1 :p
     // maybe i dont need arrays , just the day count
 
 // month-days "templates"
-  let january = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
-  let february1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
-  let february2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29]
-  let march = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
-  let april = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-  let may = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
-  let june = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-  let july = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
-  let august = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
-  let september = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-  let october = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
-  let november = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-  let december = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+  let monthsArr = {
+    january : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+    february1 : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28],
+    february2 : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29],
+    march : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+    april : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+    may : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+    june : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+    july : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+    august : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+    september : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+    october : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+    november : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+    december : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+  }
   // make these const?
-    
+
 
 // leapness check
   function February(y) {
-    
+
     if ( y % 4 == 0 ) {
 
       if ( y % 100 == 0 ) {
         if ( y % 400 == 0 ) {
           // leap year
-          return [...february2]
+          return [...monthsArr.february2]
         } else {
           // not a leap year
-          return [...february1]
+          return [...monthsArr.february1]
         }
-      
+
       } else {
-        return [...february2]
+        return [...monthsArr.february2]
       }
 
     } else {
-      return [...february1]
+      return [...monthsArr.february1]
     }
- 
+
   }
 
- 
+
 
 // get current date
   let current = {
@@ -133,7 +132,7 @@ app.post("/date", function(req, res){
  //, excessively large days
   let monthSLen
   if ( monthS == 2 ) {
-    monthSLen = February(yearS).length  //29 or 28     
+    monthSLen = February(yearS).length  //29 or 28
   } else if (monthS==4 || monthS==6 || monthS==9 || monthS==11) {
     monthSLen = 30
   } else {
@@ -141,7 +140,7 @@ app.post("/date", function(req, res){
   }
   let monthELen
   if ( monthE == 2 ) {
-    monthELen = February(yearE).length  //29 or 28     
+    monthELen = February(yearE).length  //29 or 28
   } else if (monthE==4 || monthE==6 || monthE==9 || monthE==11) {
     monthELen = 30
   } else {
@@ -198,7 +197,7 @@ app.post("/date", function(req, res){
       end.y = start.y
     }
   }
-  // swap 
+  // swap
   if (start.y > end.y){
     swap()
   }
@@ -210,12 +209,12 @@ app.post("/date", function(req, res){
   if (start.m > end.m  &&  start.y == end.y) {
     swap()
   }
- 
+
  //default
   if ( !start.d ) start.d = 1
   if ( !end.d ) {
     if ( end.m == 2 ) {
-      end.d = February(end.y).length  //29 or 28     
+      end.d = February(end.y).length  //29 or 28
     } else if (end.m==4 || end.m==6 || end.m==9 || end.m==11) {
       end.d = 30
     } else {
@@ -232,136 +231,133 @@ app.post("/date", function(req, res){
 
 
   //hold user input
-  let date = {
+  /*const?*/ let date = {
     //current: current,
     start: start,
     end: end
   }
-   
+
   console.log(date)
 
 
 
  // ---Start Adding---
 
+  let months = {}
 
   let sum = 0
 
-  for (let year=start.y; year<=end.y; year++) {
 
-    if (start.y != end.y) {
+  if ( start.y == end.y ) {    // is it a one-year case ?
 
-      if (year == start.y) {
+    sum+= addMonths ( start.m, end.m , start.y )
+    console.log(sum)      
 
-        sum+=addMonths( start.m, 12 ,year)
+  } else {     // it is a multy-year case
+
+    for (let year=start.y; year<=end.y; year++) {
+      console.log( year)
       
-      } else if (year > start.y  &&  year < end.y) {
+      if (start.y != end.y)     
 
-        sum+= addYear( year )
+        if (year == start.y) {    // is this one the first year ?
 
-      } else if (year == end.y) {
+          sum+=addMonths( start.m, 12 ,year)
+          console.log(sum)
 
-        sum+=addMonths( 1, end.m , year)
-      }
+        } else if (year > start.y  &&  year < end.y) {   // is this a "middle" year ?
 
-    } else if ( start.y == end.y ) {
-        
-      sum+= addMonths ( start.m, end.m , year )
-    
-    } /* else 
-      some monstrous error*/
+          sum+= addYear( year )
+          console.log(sum)        
 
-  }
+        } else if (year == end.y) {     // is this the final year ?
+
+          sum+=addMonths( 1, end.m , year)
+          console.log(sum)
+
+        }/* else
+        some monstrous error*/
+
+    }
+
+  } /* else
+    some monstrous error*/
+
 
   function addMonth (m, s_y,e) {     // a month to add given    1. start and end days or  2. current year
    // either s_y is start day and e is end day  or
    // s_y is year and e is not present
     let monthTotal = 0
 
-    let months = {
-      1: january.length,
-      2: February(s_y).length,
-      3: march.length,
-      4: april.length,
-      5: may.length,
-      6: june.length,
-      7: july.length,
-      8: august.length,
-      9: september.length,
-      10: october.length,
-      11: november.length,
-      12: december.length
-    }
-  
     if ( !e ) {      // if only 2 params are present then add the whole month  (2nd param would be year)
       monthTotal = months[m]
       return monthTotal
     }
 
     monthTotal = e - s_y
-  
-    return monthTotal  
+    return monthTotal
   }
 
-  function addMonths(s,e, year) {   // months to add given a start and an end ones; from the start or end year
+  function addMonths(s,e, year) {    // months to add given a start and an end ones; from the start or end year
     let subsum = 0
-    let sDay
-    let eDay
+    let sDay = undefined
+    let eDay = undefined
 
-    for ( let j=s; j<=e; j++) {
-console.log(j+"\t"+s+"\t"+e+"\t"+subsum+"\t"+sDay, eDay+"\t"+start.m+"\t"+end.m )
+    months = {
+      1: monthsArr.january.length,
+      2: February(year).length,   // param should be (year) ?
+      3: monthsArr.march.length,
+      4: monthsArr.april.length,
+      5: monthsArr.may.length,
+      6: monthsArr.june.length,
+      7: monthsArr.july.length,
+      8: monthsArr.august.length,
+      9: monthsArr.september.length,
+      10: monthsArr.october.length,
+      11: monthsArr.november.length,
+      12: monthsArr.december.length
+    }
 
-      if (start.m != end.m  &&  start.y != end.y) {
-      // if ( s != e  &&  year != end.y)
+    if (s == e  &&  start.y==end.y) {      // is it the one-month case ?
+
+      sDay = start.d
+      eDay = end.d
+      subsum+= addMonth (s,sDay,eDay)
+      console.log(s+"\t"+s+"\t"+e+"\t"+subsum+"\t"+sDay, eDay+"\t"+start.m+"\t"+end.m )
       
-        if (j == start.m  && year == start.y) {     // first month of start year
+    } else {      // it is a multy-month case
+
+      for ( let j=s; j<=e; j++) {
+        sDay = undefined
+        eDay = undefined
+
+        if (j == start.m  && year == start.y) {    // is this the first month of the first year ?
           sDay = start.d
           if ( j == 2 ) {
-            eDay = February(year).length  //29 or 28     
+            eDay = months[2]  //29 or 28
           } else if (j==4 || j==6 || j==9 || j==11) {
             eDay = 30
           } else {
             eDay = 31
           }
+
           subsum+= addMonth(j,sDay,eDay)
-        } else if (j == end.m  && year == end.y) {     // last month of end year
+          console.log(j+"\t"+s+"\t"+e+"\t"+subsum+"\t"+sDay, eDay+"\t"+start.m+"\t"+end.m )
+
+        } else if (j == end.m  && year == end.y) {   // is this the last month of the last year ?
           //sDay = 1
-          sDay = 0        // because we have to count the difference between eDay and the last month's last day , not this month's first
+          sDay = 0      // because we have to count the difference between eDay and the last month's last day , not this month's first
           eDay = end.d
+
           subsum+= addMonth(j, sDay,eDay)
-        } else {                   // some month after the first one of start year  or  some month from end year before the end month
+          console.log(j+"\t"+s+"\t"+e+"\t"+subsum+"\t"+sDay, eDay+"\t"+start.m+"\t"+end.m )
+
+        } else {        // then this must be a "middle" month - one after the first of start-year or before the last of the end-year
           subsum+= addMonth(j, year)
+          console.log(j+"\t"+s+"\t"+e+"\t"+subsum+"\t"+sDay, eDay+"\t"+start.m+"\t"+end.m )
+
         }
-      
-      } else if (start.m==end.m  &&  start.y==end.y) {
-
-        sDay = start.d
-        eDay = end.d
-        subsum+= addMonth (j,sDay,eDay)
-
-
-      } else if ( start.m != end.m  &&  start.y == end.y ) {
-        if (j == start.m) {
-          sDay = start.d
-          if ( j == 2 ) {
-            eDay = February(year).length  //29 or 28     
-          } else if (j==4 || j==6 || j==9 || j==11) {
-            eDay = 30
-          } else {
-            eDay = 31
-          }
-          subsum+= addMonth(j,sDay,eDay)
-        } else if (j == end.m ) {
-          sDay = 0
-          eDay = end.d
-          subsum+= addMonth(j, sDay,eDay)
-        } else {
-          subsum+= addMonth(j, year)
-        }
-      } //???else if ( start.m == end.m  && start.y != end.y )
-               
-               
-
+      }
 
 
     }
@@ -369,7 +365,7 @@ console.log(j+"\t"+s+"\t"+e+"\t"+subsum+"\t"+sDay, eDay+"\t"+start.m+"\t"+end.m 
     return subsum
   }
 
-  function addYear (year) {    // an entire year to add (not the first, not the last)
+  function addYear (year) {   // an entire year to add (not the first, not the last)
 
     let leap = February(year)==29
     let yearTotal
